33128aafa8e4811d8cf37740d1c11d5c2407b5ef
maksim0840 <117035413+maksim0840@users.noreply.github.com>
2024-01-14 01:49:56 +0300

    Create main.cpp
     creating window -1

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <Windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+	// WINAPI - соглашение на вызов STDCALL
+	// hInstance - дескриптор экзампляра объетка (содержаит мета данные о программе) - адресс в памяти на начало сигнатуры
+	// hPrevInstance - устарер (испольщовался для 16 бит системы)
+	// pCmdLine - аргументы командной строки
+	// nCmdShow - кол-во аргументов которые пришли с командной строки (минимум 1)
+
+	MSG msg; // стуктура с информацией о сообщении
+	HWND hwnd; // дескриптор окна (информация об окне)
+	WNDCLASSEX wc; // стуктура с характристикой окна
+
+	// ЗАПОЛНЯЕМ ПОЛЯ СТРУКТУРЫ
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.cbClsExtra = 0; // дополнительное выделение памяти в класе окна (с.да засунуть размер того что запихивать)
+	wc.cbWndExtra = 0; // дополнительное выделение памяти в класе окна (с.да засунуть размер того что запихива
+	wc.hbrBackground =  (HBRUSH)(COLOR_WINDOW + 1);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW); // handle курсора
+	wc.hInstance = hInstance; // мета данные об окне
+	wc.hIcon = LoadIcon(hInstance, IDI_APPLICATION); // ИКОНКА ПРИЛОЖЕНИЯ СНИЗУ НА ДИСПЕТЧЕРЕ (не файла)
+	wc.hIconSm = LoadIcon(hInstance, IDI_APPLICATION); // иконка в заголовке окна
+	wc.lpfnWndProc = DefWindowProc;
+	wc.lpszClassName = L"MyWindow"; // имя класса
+	wc.lpszMenuName = NULL; // указатель на имя меню
+	wc.style = CS_VREDRAW | CS_HREDRAW; // стили окна (комбинация флагов (больше флагов можно посмотреть в документации())
+
+	if (!RegisterClassEx(&wc)) { //регистрация окна в системе + проверка
+		return EXIT_FAILURE; // если создать окно не удалось
+	}
+
+	hwnd = CreateWindowEx(WS_EX_OVERLAPPEDWINDOW, L"MyWindow", L"WindowTitle", WS_OVERLAPPEDWINDOW, 
+		CW_USEDEFAULT, CW_USEDEFAULT, 600, 400, NULL, NULL, hInstance, NULL); // параметр класса совпадает с названным ранее
+	if (!hwnd) {
+		return EXIT_FAILURE;
+	}
+
+	ShowWindow(hwnd, nCmdShow); // показать окно
+	UpdateWindow(hwnd); // обновить окно
+
+	while (GetMessage(&msg, NULL, 0, 0) > 0) { // цикл, обрабатывабщий вызовы
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return EXIT_SUCCESS;
+
+}
+	
